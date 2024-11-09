d572be9907a654358a5ce006ad45909c3c741c17
rave-roses <odekunlewaris01@gmail.com>
2024-11-09 17:53:07 +0100

    added

--- hotel/src/components/ArrayPart.jsx
+++ hotel/src/components/ArrayPart.jsx
@@ -5,8 +5,9 @@ import { HiMiniArrowsPointingOut } from "react-icons/hi2";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination, Autoplay } from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export default function ArrayPart() {
   const slidesData = [
